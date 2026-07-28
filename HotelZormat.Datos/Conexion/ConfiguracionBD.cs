e286b5783d0313895d750806cb03a71155c0ab48
Jvven <jevenssenmartinezm24@gmail.com>
2026-07-27 23:34:53 -0400

    Conexiones y Criterio 3 completado

---
+++ HotelZormat.Datos/Conexion/ConfiguracionBD.cs
@@ -0,0 +1,17 @@
+﻿// Cedula : 402-1937000-0
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelZormat.Datos.Conexion
+{
+    public static class ConfiguracionBD
+    {
+        public static string ObtenerConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["HotelZormatDB"].ConnectionString;
+        }
+    }
+}
