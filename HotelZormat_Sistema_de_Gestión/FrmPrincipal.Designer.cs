ae3679a6237b8ba7df36b2df218ef588039ae96c
Jvven <jevenssenmartinezm24@gmail.com>
2026-07-27 21:58:18 -0400

    Cambio de nombres

--- HotelZormat_Sistema_de_Gestión/FrmPrincipal.Designer.cs
+++ HotelZormat_Sistema_de_Gestión/FrmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace HotelZormat_Sistema_de_Gestión
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmPrincipal";
+            this.Text = "Hotel Zormat — Sistema de Gestión";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
