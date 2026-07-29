5db3058f60520c0ad9ec1ae5a2f03b52d6430bf3
Jvven <jevenssenmartinezm24@gmail.com>
2026-07-28 20:55:26 -0400

    feat(datos): implementar UsuarioRepository y BitacoraRepository

--- HotelZormat.Negocio/Servicios/UsuarioService.cs
+++ HotelZormat.Negocio/Servicios/UsuarioService.cs
@@ -1,20 +1,50 @@
 ﻿// Cedula : 402-1937000-0
+using HotelZormat.Datos.Repositorio;
+using HotelZormat.Modelo;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using HotelZormat.Datos.Repositorio;
 
 namespace HotelZormat.Negocio.Servicios
 {
     public class UsuarioService
     {
-        private readonly IUsuarioRepository _repo;
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IBitacoraRepository _bitacoraRepository;         
 
-        public UsuarioService(IUsuarioRepository repo)
+        public UsuarioService(IUsuarioRepository usuarioRepository, IBitacoraRepository bitacoraRepository)
         {
-            _repo = repo;
+            _usuarioRepository = usuarioRepository;
+            _bitacoraRepository = bitacoraRepository;
+        }
+
+        public Usuario IniciarSesion(string nombreUsuario, string contrasena)
+        {
+            if (string.IsNullOrWhiteSpace(nombreUsuario) || string.IsNullOrWhiteSpace(contrasena))
+            {                                                              
+                return null;
+            }
+
+            Usuario usuario = _usuarioRepository.BuscarPorNombreUsuario(nombreUsuario); 
+
+            if (usuario == null)
+            {                                                           
+                return null;
+            }
+
+            if (contrasena == usuario.Contrasena)
+            {                                                              
+                SesionActual.UsuarioLogueado = usuario;
+              
+
+                _bitacoraRepository.Registrar(usuario.Id, "Login", "Inicio de sesión exitoso");  
+
+                return usuario;                                        
+            }
+
+            return null;                                                   
         }
     }
 }
